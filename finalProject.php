2fcd3777e6dd9f1fc039527f71887639ca6ac753
Collin James <mail@collinjam.es>
2016-06-01 11:13:09 -0400

    added success message

--- finalProject.php
+++ finalProject.php
@@ -11,6 +11,10 @@ $mysqli = new mysqli("oniddb.cws.oregonstate.edu","jamesc2-db","moS6V4cjzEttrXwo
 if($mysqli->connect_errno){
 	echo "Connection error " . $mysqli->connect_errno . " " . $mysqli->connect_error;
 	}
+else {
+	echo "mysqli success";
+}
+
 ?>
 
 <!DOCTYPE html>
